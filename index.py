f05be1302d95903f70919dbd7fed7dafc4882022
N i k i t a L a z a r e v <98647265+Azazzele@users.noreply.github.com>
2025-08-04 14:27:05 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns  
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+
+
+# --- 1. Указываем URL датасета ---
+URL = 'https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data'
+df = pd.read_csv(URL, header=None)
+
+print(df.shape)
+
+param_grid = {
+	'n_estimators': [100, 200, 500],
+	'max_depth':[None, 10, 20, 30],
+	'min_samples_split': [2, 5, 10]
+}
+
+headers = [
+    'word_freq_make', 'word_freq_address', 'word_freq_all', 'word_freq_3d', 
+    'word_freq_our', 'word_freq_over', 'word_freq_remove', 'word_freq_internet',
+    'word_freq_order', 'word_freq_mail', 'word_freq_receive', 'word_freq_will',
+    'word_freq_people', 'word_freq_report', 'word_freq_addresses', 'word_freq_free',
+    'word_freq_business', 'word_freq_email', 'word_freq_you', 'word_freq_credit',
+    'word_freq_your', 'word_freq_font', 'word_freq_000', 'word_freq_money',
+    'word_freq_hp', 'word_freq_hpl', 'word_freq_george', 'word_freq_650',
+    'word_freq_lab', 'word_freq_labs', 'word_freq_telnet', 'word_freq_857',
+    'word_freq_data', 'word_freq_415', 'word_freq_85', 'word_freq_technology',
+    'word_freq_1999', 'word_freq_parts', 'word_freq_pm', 'word_freq_direct',
+    'word_freq_cs', 'word_freq_meeting', 'word_freq_original', 'word_freq_project',
+    'word_freq_re', 'word_freq_edu', 'word_freq_table', 'word_freq_conference',
+    'char_freq_;', 'char_freq_(', 'char_freq_[', 'char_freq_!', 'char_freq_$',
+    'char_freq_#', 'capital_run_length_average', 'capital_run_length_longest',
+    'capital_run_length_total', 'spam'
+]
+
+df.columns = headers
+print(df.head())
+
+# Feature matrix and target vector
+X = df.drop('spam', axis=1)
+y = df['spam']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+grid_search = GridSearchCV(RandomForestClassifier(random_state=42), param_grid, cv=5, n_jobs=-1)
+grid_search.fit(X_train, y_train)
+print("Лучшие параметры:", grid_search.best_params_)
+
+model = grid_search.best_estimator_
+
+best_model = grid_search.best_estimator_
+y_pred = model.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(6,5))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Not Spam', 'Spam'], yticklabels=['Not Spam', 'Spam'])
+plt.xlabel('Predicted Class')
+plt.ylabel('True Class')
+plt.title('Confusion Matrix')
+plt.show()
+
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
